d11cfc752f429b57a45efdfe709d9187714c9c18
Blaise Johnson <johnblai@isu.edu>
2019-02-20 20:10:10 -0700

    Implement InOrderTraversal

--- src/main/java/edu/isu/cs/cs3308/traversals/InOrderTraversal.java
+++ src/main/java/edu/isu/cs/cs3308/traversals/InOrderTraversal.java
@@ -4,16 +4,30 @@ package edu.isu.cs.cs3308.traversals;
 
 import edu.isu.cs.cs3308.structures.Node;
 import edu.isu.cs.cs3308.structures.Tree;
+import edu.isu.cs.cs3308.structures.impl.LinkedBinaryTree.BinaryTreeNode;
 
 import java.util.List;
 
 public class InOrderTraversal<E> extends DepthFirstTraversal<E> {
 
-    InOrderTraversal(Tree<E> tree) {
+
+    InOrderTraversal(Tree<E> tree) throws IllegalArgumentException {
         super(tree);
     }
 
     public void subtree(Node<E> parent, List<Node<E>> snapshot) {
+        if (parent == null || snapshot == null) throw new IllegalArgumentException();
+        BinaryTreeNode<E> node = (BinaryTreeNode<E>) parent;
 
+        if (node.getLeft() != null) {
+            subtree(node.getLeft(), snapshot);
+        }
+
+        snapshot.add(node);
+        if (command != null) command.execute(tree, node);
+
+        if (node.getRight() != null) {
+            subtree(node.getRight(), snapshot);
+        }
     }
 }
--- src/main/java/edu/isu/cs/cs3308/traversals/PreOrderTraversal.java
+++ src/main/java/edu/isu/cs/cs3308/traversals/PreOrderTraversal.java
@@ -1,5 +1,3 @@
-//TODO: Implement PreOrderTraversal
-
 package edu.isu.cs.cs3308.traversals;
 
 import edu.isu.cs.cs3308.structures.Node;
